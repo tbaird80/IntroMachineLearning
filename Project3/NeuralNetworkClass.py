1aeb00e97a570c2856af08ba9d539f5b3c58c523
Tommy Baird <thomas.baird18@gmail.com>
2024-07-22 21:41:58 -0400

    more testing proj 3

--- Project3/NeuralNetworkClass.py
+++ Project3/NeuralNetworkClass.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import random
+import copy
 
 class NNet:
     def __init__(self, dataSetName, isRegression, trainingData, normalCols, numHiddenLayers, numHiddenLayerNodes, networkType):
@@ -18,6 +19,7 @@ class NNet:
 
         # init the traversal variables
         self.network = pd.DataFrame()
+        self.autoencoder = pd.DataFrame()
         self.inputNames = trainingData.columns
 
         # create input layer
@@ -25,40 +27,60 @@ class NNet:
         inputNodeNames.remove("Class")
         for currentInputName in inputNodeNames:
             inputLayerID = 0
-            self.createNode(nodeName=currentInputName, nodeLayer=inputLayerID, weightMap = {})
+            self.createNode(nodeName=currentInputName, nodeLayer=inputLayerID, weightMap={})
+            self.createNodeAutoencoder(nodeName=currentInputName, nodeLayer=inputLayerID, weightMap={})
 
         # create hidden layers
         for currentHiddenLayer in range(0, numHiddenLayers):
             currentLayerID = currentHiddenLayer + 1
-            newWeightMap = {}
 
+            newWeightMap = {'Intercept': random.uniform(-.01, .01)}
             prevNodeNames = self.network[self.network['nodeLayer'] == (currentLayerID - 1)]['nodeName'].tolist()
             for currentPrevName in prevNodeNames:
-                newWeightMap[currentPrevName] = .1
+                newWeightMap[currentPrevName] = random.uniform(-.01, .01)
 
             for currentHiddenNode in range(0, numHiddenLayerNodes):
                 currentNodeName = 'HiddenNode-' + str(currentHiddenNode) + "-" + str(currentHiddenLayer)
                 self.createNode(nodeName=currentNodeName, nodeLayer=currentLayerID, weightMap=newWeightMap)
 
+                if currentHiddenNode == 1:
+                    self.createNodeAutoencoder(nodeName=currentNodeName, nodeLayer=currentLayerID, weightMap=newWeightMap)
+
         # create output layer
         outputLayerID = numHiddenLayers + 1
 
         # create mapping to previous layer with weights randomly created starting with intercept
-        newWeightMap = {'Intercept': random.uniform(-.01, .01)}
+        outputWeightMap = {'Intercept': random.uniform(-.01, .01)}
         prevNodeNames = self.network[self.network['nodeLayer'] == (outputLayerID - 1)]['nodeName'].tolist()
         for currentPrevName in prevNodeNames:
-            newWeightMap[currentPrevName] = random.uniform(-.01, .01)
+            outputWeightMap[currentPrevName] = random.uniform(-.01, .01)
 
         if self.isReg:
-            self.createNode(nodeName='OutputNode1', nodeLayer=outputLayerID, weightMap=newWeightMap)
+            possibleOutputs = ['OutputNode1']
         else:
             possibleOutputs = self.trainData['Class'].unique()
-            for currentOutput in possibleOutputs:
-                if self.isReg:
-                    currentNodeName = 'OutputNode'
-                else:
-                    currentNodeName = currentOutput
-                self.createNode(nodeName=currentNodeName, nodeLayer=outputLayerID, weightMap=newWeightMap)
+
+        for currentOutput in possibleOutputs:
+            if self.isReg:
+                currentNodeName = 'OutputNode'
+            else:
+                currentNodeName = currentOutput
+            self.createNode(nodeName=currentNodeName, nodeLayer=outputLayerID, weightMap=outputWeightMap)
+
+        # output layer is 2 in autoencoder, they match the inputs
+        outputLayerID = 2
+        inputNodeNames = list(self.trainData.columns)
+        inputNodeNames.remove("Class")
+        for currentInputName in inputNodeNames:
+            # create mapping to previous layer with weights randomly created starting with intercept
+            outputWeightMap = {'Intercept': random.uniform(-.01, .01)}
+            prevNodeNames = self.network[self.network['nodeLayer'] == (outputLayerID - 1)]['nodeName'].tolist()
+
+            # iterate through the previous layer names
+            for currentPrevName in prevNodeNames:
+                outputWeightMap[currentPrevName] = random.uniform(-.01, .01)
+
+            self.createNodeAutoencoder(nodeName=currentInputName, nodeLayer=outputLayerID, weightMap=outputWeightMap)
 
     def createNode(self, nodeName, nodeLayer, weightMap):
         newRow = pd.DataFrame({
@@ -72,6 +94,27 @@ class NNet:
         }, index=[len(self.network)])
 
         self.network = pd.concat([self.network, newRow])
+
+    def createNodeAutoencoder(self, nodeName, nodeLayer, weightMap):
+        newRow = pd.DataFrame({
+            'nodeName': [nodeName],
+            'nodeLayer': [nodeLayer],
+            'weightMap': [weightMap],
+            'currentInputs': [[]],
+            'currentOutputs': [[]],
+            'actualValue': [[]],
+            'currentPartialError': [[]]
+        }, index=[len(self.autoencoder)])
+
+        self.autoencoder = pd.concat([self.autoencoder, newRow])
+
+    def updateWithAutoencoder(self):
+        # find the first two layers of autoencoder, and second two layers of regular network
+        firstTwoLayers = self.autoencoder[self.autoencoder['nodeLayer'] < 2]
+        lastTwoLayers = self.network[self.network['nodeLayer'] > 1]
+
+        # concat the two sections together
+        self.network = pd.concat([firstTwoLayers, lastTwoLayers], axis=0)
 
     def forwardPass(self, currentInputBatch):
         # iterate through all passed in records
@@ -143,10 +186,12 @@ class NNet:
                         softmaxValue = softmaxValue / totalSoftmax
                         self.network.loc[nodeID, 'currentOutputs'].append(softmaxValue)
 
-    def updatePartialErrors(self):
+    def updatePartialErrors(self, indexStoppingPoint=0):
         # iterate through our network backwards
         currentLayer = self.numHiddenLayers + 1
-        while currentLayer > 0:
+
+        # check layer to stop updating at
+        while currentLayer > indexStoppingPoint:
             # find our current layer of nodes
             currentLayerNodes = self.network[self.network['nodeLayer'] == currentLayer]
             nextLayerNodes = self.network[self.network['nodeLayer'] == currentLayer + 1]
@@ -174,7 +219,7 @@ class NNet:
             # move back one layer
             currentLayer -= 1
 
-    def updateWeights(self):
+    def updateWeights(self, indexStoppingPoint=0):
 
         # create our learning rate as proportion of current set size (proxy size of output from first record) to entire training set
         currentTestSize = len(self.network['currentOutputs'].iloc(0))
@@ -183,7 +228,7 @@ class NNet:
 
         # iterate through our network backwards
         currentLayer = self.numHiddenLayers + 1
-        while currentLayer > 0:
+        while currentLayer > indexStoppingPoint:
             # iterate through our current layer nodes
             currentLayerNodes = self.network[self.network['nodeLayer'] == currentLayer]
             for currentNode in currentLayerNodes.index.tolist():
@@ -206,8 +251,11 @@ class NNet:
             currentLayer -= 1
 
         # reset out output values once our weights have been updated
+        self.dropLastForwardPass()
+
+    def dropLastForwardPass(self):
+        # reset out output values once our weights have been updated
         self.network['currentInputs'] = [[]]
         self.network['currentOutputs'] = [[]]
         self.network['actualValue'] = [[]]
         self.network['currentPartialError'] = [[]]
-
